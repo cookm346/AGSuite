30248396a46c5910ad37d784447300816c973ccf
Matt Cook <cookm346@myumanitoba.ca>
2016-10-06 16:16:31 -0500

    changed order of predictors in theworks()

--- R/TheWorks.R
+++ R/TheWorks.R
@@ -9,32 +9,33 @@
 theworks <- function(train, test){
     sl <- strlength(test)
     le <- legalentry(train, test)
+    
     n2 <- novelty(train, test, n_gram = 2, proportion = FALSE)
     n3 <- novelty(train, test, n_gram = 3, proportion = FALSE)
+    cn <- n2 + n3
+    
     ncp2 <- novelty(train, test, n_gram = 2, proportion = TRUE)
     ncp3 <- novelty(train, test, n_gram = 3, proportion = TRUE)
-    
-    cn <- n2 + n3
     ncp <- ncp2 + ncp3
     
     gacs2 <- acs(train, test, n_gram = 2, anchor = FALSE)
     gacs3 <- acs(train, test, n_gram = 3, anchor = FALSE)
+    gacs <- gacs2 + gacs3
+    
     aacs2 <- acs(train, test, n_gram = 2, anchor = TRUE)
     aacs3 <- acs(train, test, n_gram = 3, anchor = TRUE)
-    
-    gacs <- gacs2 + gacs3
     aacs <- aacs2 + aacs3
-    
+
     ldmean <- levdist(train, test, measure = "mean")
     ldmedian <- levdist(train, test, measure = "median")
     ldmin <- levdist(train, test, measure = "min")
     ldmax <- levdist(train, test, measure = "max")
     
     
-    m <- cbind(sl, le, n2, n3, ncp2, ncp3, cn, ncp, gacs2, gacs3, 
-                 aacs2, aacs3, gacs, aacs, ldmean, ldmedian, ldmin, ldmax)
+    m <- cbind(sl, le, n2, n3, cn, ncp2, ncp3, ncp, gacs2, gacs3, gacs,
+                 aacs2, aacs3, aacs, ldmean, ldmedian, ldmin, ldmax)
     
-    colnames(m)[c(7, 8, 13, 14)] <- c("Chunk Novelty", "Novel Chunk Proportion", "Global ACS", "Anchor ACS")
+    colnames(m)[c(5, 8, 11, 14)] <- c("Chunk Novelty", "Novel Chunk Proportion", "Global ACS", "Anchor ACS")
     
     return(m)
 }
