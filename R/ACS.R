3bec90baada166843ecee798d0d83869dd9e9cc2
Matt Cook <cookm346@myumanitoba.ca>
2016-12-29 11:59:58 -0600

    made correction to ACS function

--- R/ACS.R
+++ R/ACS.R
@@ -10,62 +10,83 @@
 
 #ACS FUNCTION
 acs <- function(train, test, n_gram = 2, anchor = FALSE){
+    
 
-    #generate matrix to store the training grams
-    g <- matrix(" ", length(train), (max(nchar(train))) - (n_gram - 1))
-
-    #extract all training strings grams and save them in matrix g
-    for(i in 1:(length(train))){
-        for(j in 1:(nchar(train[i]) - (n_gram - 1))){
-            g[i, j] <- substring(train[i], j, (j + (n_gram - 1)))
+    #HELPER FUNCTIONS
+    
+    #get all n-grams from vector
+    grams <- function(x, n_grams){
+        #generate matrix to store the training grams
+        grams <- matrix(" ", length(x), (max(nchar(x))) - (n_gram - 1))
+        #extract all training strings grams and save them in matrix grams
+        for(i in 1:(length(x))){
+            for(j in 1:(nchar(x[i]) - (n_gram - 1))){
+                grams[i, j] <- substring(x[i], j, (j + (n_gram - 1)))
+            }
         }
+        return(grams)
     }
 
-    #if anchor is set to TRUE
-    # extract only first and last gram from each training string
+    #get all achor grams from vector of grams
+    anchorGrams <- function(grams){
+        anchor_grams <- matrix(0, nrow(grams), 2)
+        for(i in 1:nrow(grams)){
+            
+            current_row <- grams[i, ]
+            anchor_grams[i, 1] <- current_row[1]
+            current_row_grams <- current_row[!(current_row == " ")]
+            anchor_grams[i, 2] <- current_row_grams[length(current_row_grams)]
+        }
+        return(anchor_grams)
+    }
+
+    #extract n-grams, convert to vector, remove empty elements
+    train_grams <- grams(train, 2)
     if(anchor == TRUE){
-
-        a <- matrix(0, (length(train) * 2))
-
-        for(i in 1:length(train)){
-
-            a[i] <- g[i]
-            a[i + length(train)] <- g[nchar(train[i]) - (n_gram - 1)]
-        }
-
-        g <- a
+        train_grams <- anchorGrams(train_grams)
     }
-
-    #count occurance of test grams observed in training grams
+    train_grams <- c(train_grams)
+    train_grams <- train_grams[!(train_grams == " ")]
+    
+    
+    test_grams <- grams(test, 2)
+    if(anchor == TRUE){
+        test_grams <- anchorGrams(test_grams)
+    }
+    
+    
     acs <- matrix(0, length(test))
-
-    #for each test string count the number of n gram matches
-    for(i in 1:(length(test))){
+    
+    for(i in 1:nrow(test_grams)){
         count <- 0
-        for(j in 1:(nchar(test[i]) - (n_gram - 1))){
-            count <- count + sum(substring(test[i], j, (j + (n_gram - 1))) == g)
+        for(j in 1:ncol(test_grams)){
+            count <- count + sum(train_grams %in% test_grams[i,j] == TRUE)
         }
         acs[i] <- count
     }
-
+    
+    
+    #divide anchor acs by 2
+    #divide global acs by number of grams of each test item
     if(anchor == TRUE){
         acs <- acs / 2
-        acs[which(nchar(test) == n_gram)] <- acs[which(nchar(test) == n_gram)] * 2
         a_name <- "Anchor"
     } else {
-        acs <- acs / nchar(test)
+        acs <- acs / apply(test_grams, 1, function(test_grams){length(test_grams[!(test_grams == " ")])})
         a_name <- "Global"
     }
-
-
+    
+    
     rownames(acs) <- test
-
+    
     if(n_gram == 2)  colnames(acs) <- paste(a_name, "Bigram ACS", sep = " ")
     if(n_gram == 3)  colnames(acs) <- paste(a_name, "Trigram ACS", sep = " ")
     if(n_gram == 4)  colnames(acs) <- paste(a_name, "4-gram ACS", sep = " ")
     if(n_gram == 5)  colnames(acs) <- paste(a_name, "5-gram ACS", sep = " ")
-
+    
+    
     return(acs)
+    
 }
 
 
