e162f1a7b8ec098f4964307971c3812c03fac6d9
Matt Cook <cookm346@myumanitoba.ca>
2017-03-24 15:48:16 -0500

    added pattern (abstract analogy) measure

---
+++ R/Pattern.R
@@ -0,0 +1,47 @@
+#' Pattern Matching
+#'
+#' Computes the number of times the pattern of the test string (regardless of surface structure) appears in training strings
+#' @param train a vector of training strings
+#' @param test a vector of test strings
+#' @return a vector containing the number of pattern matches that occur between the test item and all training items
+#' @export
+
+
+pattern <- function(train, test){
+
+    ptrn <- matrix(0, length(test))
+
+    for(i in 1:length(test)){
+        ptrn[i] <- Repetition_match(test[i], train)
+    }
+
+    rownames(ptrn) <- test
+    colnames(ptrn) <- "Pattern Matches"
+
+    return(ptrn)
+}
+
+
+Repetition <- function (x) {
+    x <- gsub("^\\s+|\\s+$", "", x)
+    n <- matrix(0, nchar(x))
+    UniqueSymbols <- unique(strsplit(x, "")[[1]])
+    for (i in 1:nchar(x)) {
+        for (j in 1:length(UniqueSymbols)) {
+            if (substr(x, i, i) == UniqueSymbols[j]) n[i] <- j
+        }
+    }
+    return(paste(n, collapse=""))
+}
+
+# This function counts the number of exact matches
+# between a string "Probe" and all items in a
+# vector "Memory". It uses Repetition above
+
+Repetition_match <- function (Probe, Memory) {
+    n <- 0
+    for (i in 1:length(Memory)) {
+        if (Repetition(Probe) == Repetition(Memory[i])) n <- n + 1
+    }
+    return(n)
+}
--- R/TheWorks.R
+++ R/TheWorks.R
@@ -9,19 +9,19 @@
 theworks <- function(train, test){
     sl <- strlength(test)
     le <- legalentry(train, test)
-    
+
     n2 <- novelty(train, test, n_gram = 2, proportion = FALSE)
     n3 <- novelty(train, test, n_gram = 3, proportion = FALSE)
     cn <- n2 + n3
-    
+
     ncp2 <- novelty(train, test, n_gram = 2, proportion = TRUE)
     ncp3 <- novelty(train, test, n_gram = 3, proportion = TRUE)
     ncp <- ncp2 + ncp3
-    
+
     gacs2 <- acs(train, test, n_gram = 2, anchor = FALSE)
     gacs3 <- acs(train, test, n_gram = 3, anchor = FALSE)
     gacs <- gacs2 + gacs3
-    
+
     aacs2 <- acs(train, test, n_gram = 2, anchor = TRUE)
     aacs3 <- acs(train, test, n_gram = 3, anchor = TRUE)
     aacs <- aacs2 + aacs3
@@ -30,18 +30,20 @@ theworks <- function(train, test){
     ldmedian <- levdist(train, test, measure = "median")
     ldmin <- levdist(train, test, measure = "min")
     ldmax <- levdist(train, test, measure = "max")
-    
+
     r <- redundancy(test)
-    
-    
+
+    p <- pattern(train, test)
+
+
     m <- cbind(sl, le, ldmin, ldmean, ldmedian, ldmax, gacs2, gacs3, gacs, aacs2, aacs3, aacs,
-               n2, n3, cn, ncp2, ncp3, ncp, r)
-    
-    
+               n2, n3, cn, ncp2, ncp3, ncp, r, p)
+
+
     colnames(m) <- c("String Length", "Legal Entry", "Min Levenshtein", "Mean Levenshtein", "Median Levenshtein", "Max Levenshtein",
                      "Global Bigram ACS", "Global Trigram ACS", "Global ACS", "Anchor Bigram ACS",
                      "Anchor Trigram ACS", "Anchor ACS", "Bigram Novelty", "Trigram Novelty",
-                     "Chunk Novelty", "Bigram NCP", "Trigram NCP", "NCP", "First-Order Redundancy")
-    
+                     "Chunk Novelty", "Bigram NCP", "Trigram NCP", "NCP", "First-Order Redundancy", "Pattern Matches")
+
     return(m)
 }
